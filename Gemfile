83e76064e5d890ea999ae5fc2f438c212fdfc52b
ryanstgermain <ryanjoel26@gmail.com>
2020-01-08 12:18:08 -0700

    Added validations for users, and finished functionality.

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'puma', '~> 3.11'
 gem 'bootstrap', '~> 4.4.1'
 gem 'jquery-rails'
 gem 'sass-rails', '~> 5.0'
+gem 'will_paginate', '3.2.1'
+gem 'bootstrap-will_paginate', '1.0.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_175130) do
+ActiveRecord::Schema.define(version: 2020_01_08_190443) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_01_08_175130) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
