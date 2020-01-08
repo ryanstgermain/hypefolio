15c08b1c012a240f8b10b0a82ce7f64435e63481
ryanstgermain <ryanjoel26@gmail.com>
2020-01-08 15:50:02 -0700

    Initial commit.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
-    # has_many :items
+    has_many :items
     validates :username, presence: true, 
               uniqueness: { case_sensitive: false },
               length: { minimum: 3, maximum: 25 }
